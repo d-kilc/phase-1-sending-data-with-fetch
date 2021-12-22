0832368640300b683712f2343600f979bbe9ee6a
Daniel <dkilcullen25@gmail.com>
2021-12-22 09:24:54 -0600

    commit tree 622660bb9fedc9d13cdae87a71cac27606e063e3

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(obj) {
+    fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: obj.name,
+            email: obj.email
+        })
+    })
+        .then((res) => res.json())
+        .then((json) => console.log(json))
+}
+
+const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  };
+
+
+
+submitData({name: 'Dan', email: 'dan@example.com'})
